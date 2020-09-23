fee2ef41dd1da12ec67482e27a65b5c42234605a
Maxime Leduc <maxime.leduc@phantombuster.com>
2020-09-23 16:15:12 +0200

    5 Harversters with more move

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
-    if(harvesters.length < 2) {
+    if(harvesters.length < 5) {
         var newName = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE,MOVE], newName,
             {memory: {role: 'harvester'}});
     }
 
