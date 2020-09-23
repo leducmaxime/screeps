8b8776ef09fe2bb9203a8dc375519944c4fcd03b
Maxime Leduc <maxime.leduc@phantombuster.com>
2020-09-23 18:42:23 +0200

    getting harvesters with nothing to do to flag1

--- role.harvester.js
+++ role.harvester.js
@@ -21,7 +21,10 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            }
+            } else {
+            	var flags = creep.room.find(FIND_FLAGS, { filter: (flag) => flag.name == "Flag1" });
+                creep.moveTo(flags[0], {visualizePathStyle: {stroke: '#ffffff'}});
+	        }
         }
     }
 };
