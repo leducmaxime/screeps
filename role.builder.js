5c2a1fb6cd036e31c72a7a56f9c66677278e6d1e
Maxime Leduc <maxime.leduc@phantombuster.com>
2020-09-23 19:11:14 +0200

    Moving builders to source #2

--- role.builder.js
+++ role.builder.js
@@ -15,8 +15,8 @@ var roleBuilder = {
         if(creep.memory.building) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                if(creep.build(targets[1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[1], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
